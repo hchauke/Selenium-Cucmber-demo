8cea70f74dbb3aaa5802389d376f362307f137a6
Hlulani Chauke <hchauke@thoughtworks.com>
2019-02-06 15:12:53 +0200

    Added a logout page object class

---
+++ src/main/java/pageObjects/Logout.java
@@ -0,0 +1,13 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class Logout {
+
+	public WebDriver driver;
+
+	public Logout(WebDriver driver) {
+
+		this.driver = driver;
+	}
+}
